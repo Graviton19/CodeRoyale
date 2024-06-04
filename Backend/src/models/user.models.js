34eb3150f5185e0ca968ba35576a4abb36285a3d
manasgupta28 <manas.gupta@geminisolutions.com>
2024-06-04 16:12:47 +0530

    email.js and user-Modal

--- Backend/src/models/user.models.js
+++ Backend/src/models/user.models.js
@@ -1,39 +1,68 @@
-import mongoose, { Schema } from "mongoose"
+import mongoose from "mongoose";
+import bcrypt from "bcrypt";
+
+const { Schema } = mongoose;
 
 const userSchema = new Schema(
     {
-        username:{
+        username: {
             type: String,
-            required: true,
+            required: [true, "Username is required"],
             unique: true,
             lowercase: true,
             trim: true,
-            index: true
+            index: true,
+            maxlength: [50, "Username cannot exceed 50 characters"]
         },
-        password:{
+        password: {
             type: String,
-            required: true
+            required: [true, "Password is required"],
+            minlength: [6, "Password must be at least 6 characters long"],
+            maxlength: [100, "Password cannot exceed 100 characters"]
         },
-        email:{
+        email: {
             type: String,
-            required: true,
+            required: [true, "Email is required"],
             unique: true,
             lowercase: true,
             trim: true,
-        }
-    },{timestamps: true}
-)
+            validate: {
+                validator: function(v) {
+                    return /\S+@\S+\.\S+/.test(v);
+                },
+                message: props => `${props.value} is not a valid email!`
+            },
+            maxlength: [100, "Email cannot exceed 100 characters"]
+        },
+        isVerified: {
+            type: Boolean,
+            default: false
+        },
+        otp: String
+    },
+    { timestamps: true }
+);
 
-
-userSchema.pre("save",async(next)=>{
-    if(!this.isModified("password")) return next()
+userSchema.pre("save", async function(next) {
+    if (!this.isModified("password")) return next();
     
-    this.password = await bcrypt.hash(this.password,10)
-    next()
-})
+    try {
+        const hashedPassword = await bcrypt.hash(this.password, 10);
+        this.password = hashedPassword;
+        next();
+    } catch (error) {
+        next(error);
+    }
+});
 
-userSchema.methods.isPasswordCorrect = async function(password){
-    return await bcrypt.compare(password,this.passsword)
-}
+userSchema.methods.isPasswordCorrect = async function(password) {
+    try {
+        return await bcrypt.compare(password, this.password);
+    } catch (error) {
+        return false;
+    }
+};
 
-export const User = mongoose.model("User",userSchema)
+const User = mongoose.model("User", userSchema);
+
+export default User;
