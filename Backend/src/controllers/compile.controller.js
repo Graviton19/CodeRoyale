688eb74eda310d9c41c005ac34425f8bc6204f4c
Graviton19 <aalekhbukhariya@gmail.com>
2024-06-05 00:33:52 +0530

    Added compiler setup and some constant variables to work with added compile route too

---
+++ Backend/src/controllers/compile.controller.js
@@ -0,0 +1,10 @@
+import { User } from "../models/user.models.js"
+import { asyncHandler } from "../utils/asyncHandler.js"
+import { ApiResponse } from "../utils/ApiResponse.js"
+import { ApiError } from "../utils/ApiError.js"
+
+const CompileCode = asyncHandler(async(req,res)=>{
+
+})
+
+export {CompileCode}
--- Backend/src/routes/user.Routes.js
+++ Backend/src/routes/user.Routes.js
@@ -1,11 +1,14 @@
 import { Router } from "express"
 import { RegisterUser } from "../controllers/user.controller.js"
 import { sendOTP } from "../controllers/otp.controller.js"
+import { CompileCode } from "../controllers/compile.controller.js"
 const router = Router()
 
 router.route("/sendOTP").post(sendOTP)
 
 router.route("/register").post(RegisterUser)
+
+router.route("/compile").post(CompileCode)
 
 export default router
 
